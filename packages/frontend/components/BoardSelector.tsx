5e9a781643461606ef1601ba0701b1e6ef3d8cf7
Mecki-Messer <mecki.messer.gaming@web.de>
2021-06-16 17:56:05 +0200

    moved to components

--- packages/frontend/components/BoardSelector.tsx
+++ packages/frontend/components/BoardSelector.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect} from 'react'
 
-export default function BoardSelector() {
+export default function Bo../components/MessageBoard{
     return (
         <div>
             
