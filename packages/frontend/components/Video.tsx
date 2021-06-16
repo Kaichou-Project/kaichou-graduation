3650a532a7a50006f897ac80414a6f866abf2c3a
Mecki-Messer <mecki.messer.gaming@web.de>
2021-06-17 00:36:27 +0200

    modifued scss and video.tsx

--- packages/frontend/components/Video.tsx
+++ packages/frontend/components/Video.tsx
@@ -5,9 +5,8 @@ import styles from '../styles/videoBoard.module.scss'
 export default function Video({ video }) {
   return (
     <div>
-      <h3 className={styles.text}>
-        {video.title} - {video.creator}
-      </h3>
+      <h3 className={styles.cardInfo}>{video.title}</h3>
+      <h4 className={styles.cardInfo}> - {video.creator}</h4>
       <ReactPlayer className={styles.player} url={video.url} />
     </div>
   )
