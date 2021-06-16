ae9b8bc536a2440bfa455a289ee23504ff1eaf82
TakoLeakest <takoa@tako.tako>
2021-06-16 18:46:56 -0500

    Transforms Particle in a single component
    - Moves the code for the Particles to a single component (Also moving its SCSS)
    - Modifies _app to always render the SakuraParticles component
    - Deletes sakura-tree image used for testing, waiting for another one with permission

---
+++ packages/frontend/components/SakuraParticles.tsx
@@ -0,0 +1,62 @@
+import Particles from 'react-particles-js'
+import styles from '../styles/SakuraParticles.module.scss';
+
+function SakuraParticles() {
+    return <Particles
+    className={styles.particles}
+    params={{
+      particles: {
+        shape: {
+          type: 'image',
+          image: [
+            {
+              src: './sakura-particles/sakura.png',
+              width: 400,
+              height: 200,
+            },
+            {
+              src: './sakura-particles/sakura2.png',
+              width: 400,
+              height: 200,
+            },
+            {
+              src: './sakura-particles/sakura3.png',
+              width: 400,
+              height: 200,
+            },
+            {
+              src: './sakura-particles/sakura4.png',
+              width: 400,
+              height: 200,
+            },
+          ],
+        },
+        number: {
+          value: 80,
+          density: {
+            enable: false,
+          },
+        },
+        size: {
+          value: 32,
+          random: true,
+          anim: {
+            speed: 4,
+            size_min: 0.3,
+          },
+        },
+        line_linked: {
+          enable: false,
+        },
+        move: {
+          random: true,
+          speed: 0.5,
+          direction: 'bottom',
+          out_mode: 'out',
+        },
+      },
+    }}
+  />
+}
+
+export default SakuraParticles
--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -2,10 +2,14 @@
 import React from 'react'
 
 //Project imports
+import SakuraParticles from '../components/SakuraParticles'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <SakuraParticles />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
