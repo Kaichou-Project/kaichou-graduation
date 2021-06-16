9644ff14b22eaf686842bfc919609687953461a1
RedMap <redmapyt@gmail.com>
2021-06-16 09:50:57 +0200

    Fix formatting and move to components folder

--- packages/frontend/pages/MessageList.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Message from "./Message";
-
-export default function MessageList({ messages }) {
-    return (messages.map(message => <Message key={message.id} message={message}/>))
-}
