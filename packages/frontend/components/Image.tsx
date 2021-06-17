53cbeefbd65154beb954a9331babfda09ddbb744
Mecki-Messer <85892774+Mecki-Messer@users.noreply.github.com>
2021-06-17 06:00:16 +0200

    Update packages/frontend/components/Image.tsx
    Co-authored-by: Nyxordinal <85892079+nyxordinal@users.noreply.github.com>

--- packages/frontend/components/Image.tsx
+++ packages/frontend/components/Image.tsx
@@ -12,7 +12,14 @@ interface ImageProps {
   key: string
   image: ImageInterface
 }
-export default function Image({ image }) {
+export default function Image(props: ImageProps) {
+  return (
+    <div>
+      <h4>Creator: {props.image.creator}</h4>
+      <img src={props.image.url} width={300} height={250}></img>
+    </div>
+  )
+}
   return (
     <div>
       <h4>Creator: {image.creator}</h4>
