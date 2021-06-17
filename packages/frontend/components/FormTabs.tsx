11745f4540168a801a85b33263b3040bfcf446e5
yovic4 <yovic4@yahoo.co.id>
2021-06-17 22:37:03 +0700

    Add visual for form tabs

---
+++ packages/frontend/components/FormTabs.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from '../styles/Form.module.scss'
+
+export default function FormTabs() {
+  const selected = true
+  return (
+    <div className={styles.tabs}>
+      <label>I want to submit a ...</label>
+      <div className={`${styles.button} ${selected ? styles.selected : ''}`}>
+        MESSAGE
+      </div>
+      <div className={`${styles.button} ${styles.yellow}`}>FANART</div>
+      <div className={`${styles.button} ${styles.yellow}`}>VIDEO/CLIP</div>
+    </div>
+  )
+}
