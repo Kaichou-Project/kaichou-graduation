df574ba33159b6da25228a11dc0ea9055bab0ea9
keijeizei <kctan9@up.edu.ph>
2021-06-17 09:35:48 +0800

    add social media icons in footer

--- packages/frontend/components/Footer.tsx
+++ packages/frontend/components/Footer.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import styles from './Footer.module.scss';
+import styles from './Footer.module.scss'
 
 export default function Footer() {
   return (
-    <div>
+    <div className={styles.footer}>
       <div className={styles.footer_bg_container}>
         <div className={styles.bg}></div>
         <div className={styles.end}></div>
-    </div>
+      </div>
       <div className={styles.footer_container}>
-        <div className={styles.footer_title_container}>
+        <div className={styles.title}>
           <h2>Kiryu Coco Graduation Project</h2>
           <p>&copy; 2021 Kaichou Project</p>
         </div>
         <p className={styles.footer_subtitle}>Forever Kiryu Kai</p>
-        <div className={styles.footer_links_container}>
-          <div className={styles.footer_links_column}>
+        <div className={styles.links}>
+          <div className={styles.column}>
             <p>Contents</p>
             <a>Home</a>
             <a>Clips</a>
@@ -23,7 +23,7 @@ export default function Footer() {
             <a>Soundboard</a>
             <a>Credits</a>
           </div>
-          <div className={styles.footer_links_column}>
+          <div className={styles.column}>
             <p>Partners</p>
             <a>Asa Coco University</a>
             <a>Coco&apos;s Graduation Farewell</a>
@@ -31,18 +31,46 @@ export default function Footer() {
             <a>Cinderella Project</a>
             <a>Kiryukai Project</a>
           </div>
-          <div className={styles.footer_links_column}>
+          <div className={styles.column}>
             <p>Legal</p>
             <a>Terms</a>
             <a>Privacy</a>
             <a>Accessibility</a>
           </div>
         </div>
-        <div className={styles.footer_links_container}>
-          <a className={styles.footer_link_social}>Github</a>
-          <a className={styles.footer_link_social}>Twitter</a>
-          <a className={styles.footer_link_social}>Youtube</a>
-          <a className={styles.footer_link_social}>Discord</a>
+        <div className={styles.links}>
+          <a>
+            <img
+              src="/social-icons/github.svg"
+              alt="Github"
+              width="50"
+              height="50"
+            />
+          </a>
+          <a>
+            <img
+              src="/social-icons/twitter.svg"
+              alt="Github"
+              width="50"
+              height="50"
+            />
+          </a>
+          <a>
+            <img
+              src="/social-icons/youtube.svg"
+              alt="Github"
+              width="50"
+              height="50"
+            />
+          </a>
+          <a>
+            <img
+              src="/social-icons/discord.svg"
+              alt="Github"
+              width="50"
+              height="50"
+            />
+          </a>
         </div>
       </div>
     </div>
