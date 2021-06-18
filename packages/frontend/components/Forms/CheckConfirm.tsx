4124d215bc3df3581c192940e161ac39237b1660
yovic4 <yovic4@yahoo.co.id>
2021-06-18 23:43:41 +0700

    Adjusting checkbox for cursor

--- packages/frontend/components/Forms/CheckConfirm.tsx
+++ packages/frontend/components/Forms/CheckConfirm.tsx
@@ -5,10 +5,12 @@ import PropTypes from 'prop-types'
 export default function CheckConfirm({ name, error }) {
   return (
     <div className={styles.confirm}>
-      <label className={styles.check_container}>
-        <input type="checkbox" name={name} />
-        <span />
-      </label>
+      <div>
+        <label className={styles.check_container}>
+          <input type="checkbox" name={name} />
+          <span />
+        </label>
+      </div>
       <div>
         I hereby confirm that all the content contained in my submission is my
         own or i have explicit permission by the author to use them.
