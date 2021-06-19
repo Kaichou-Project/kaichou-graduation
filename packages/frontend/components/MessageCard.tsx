fa513ea534de04c48926e8d67340638813a7a336
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2021-06-18 22:13:56 -0400

    Update MessageCard.tsx

--- packages/frontend/components/MessageCard.tsx
+++ packages/frontend/components/MessageCard.tsx
@@ -4,11 +4,15 @@ import cardInfo from '../interfaces/cardInfo'
 
 export default function MessageCard(props: cardInfo) {
   const { title, message, author } = props
+  const image = '/message-card-elements/Vector3.svg'
   return (
-    <div className={styles.card}>
-      <div className={styles.title}>{title} </div>
-      <div className={styles.message}>{message} </div>
-      <div className={styles.author}>{author} </div>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.title}>{title} </div>
+        <img className={styles.img} src={image} />
+        <div className={styles.message}>{message} </div>
+        <div className={styles.author}>{author} </div>
+      </div>
     </div>
   )
 }
