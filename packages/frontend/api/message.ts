9e63081da8810c21f60bfe6d18190ff7fe228c85
yovic4 <85892325+yovic4@users.noreply.github.com>
2021-06-21 17:33:16 +0700

    Update packages/frontend/api/message.ts
    Ok, committing this
    
    Co-authored-by: RedMap <redmapyt@gmail.com>

--- packages/frontend/api/message.ts
+++ packages/frontend/api/message.ts
@@ -2,9 +2,9 @@ import { MessageInterface } from '../interfaces/message'
 import axios from 'axios'
 
 const messageInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/message`,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 })
 
 export async function createMessage(data: MessageInterface) {
-  await messageInstance.post('', data)
+  await messageInstance.post('/message', data)
 }
