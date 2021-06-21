b862607024afdc4f9bfcf24c1fb6514bb6c96faf
yovic4 <yovic4@yahoo.co.id>
2021-06-21 17:47:07 +0700

    Change env variable name

--- packages/frontend/api/message.ts
+++ packages/frontend/api/message.ts
@@ -2,7 +2,7 @@ import { MessageInterface } from '../interfaces/message'
 import axios from 'axios'
 
 const messageInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 })
 
 export async function createMessage(data: MessageInterface) {
