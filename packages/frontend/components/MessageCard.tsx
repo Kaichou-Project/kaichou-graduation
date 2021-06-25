1b27651eaf5a9b24ff2feee0e628a42b4a2aeeaf
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2021-06-25 10:43:25 -0400

    Fixed Compile issue

--- packages/frontend/components/MessageCard.tsx
+++ packages/frontend/components/MessageCard.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import styles from '../styles/MessageCard.module.scss'
-import MessageProps from '../interfaces/messageProps'
+import styles from './MessageCard.module.scss'
+import cardInfo from '../../interfaces/cardInfo'
 
-export default function MessageCard(props: MessageProps) {
+export default function MessageCard(props: cardInfo) {
   // This changes the image on the top right of the card
   const image = '/message-card-elements/cardimage.svg'
   return (
     <div className={styles.card}>
-      <div className={styles.title}>{props.cardData.title} </div>
+      <div className={styles.title}>{props.title} </div>
       <img className={styles.img} src={image} />
-      <div className={styles.message}>{props.cardData.message} </div>
-      <div className={styles.author}>{props.cardData.author} </div>
+      <div className={styles.message}>{props.message} </div>
+      <div className={styles.author}>{props.author} </div>
     </div>
   )
 }
