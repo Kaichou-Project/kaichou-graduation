1457cd56a6ed94d8ca72369b0a02837d5b4a8544
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2021-06-25 21:50:34 -0400

    Update MessageList.tsx

--- packages/frontend/components/MessageCards/MessageList.tsx
+++ packages/frontend/components/MessageCards/MessageList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MessageCard from './MessageCard'
 import MessageProps from '../../interfaces/messageProps'
 import styles from './MessageCard.module.scss'
+import StackGrid from 'react-stack-grid'
 
 export default function MessageList(props: MessageProps) {
   const cards = props.cardData.map((message, i) => (
@@ -9,7 +10,9 @@ export default function MessageList(props: MessageProps) {
   ))
   return (
     <div className={styles.container}>
-      <div className={styles.grid}>{cards}</div>
+      <StackGrid columnWidth={500} gutterWidth={30} gutterHeight={30}>
+        {cards}
+      </StackGrid>
     </div>
   )
 }
