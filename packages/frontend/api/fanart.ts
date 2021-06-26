d5b3f6e777137e219a7077a3417ec9e9e80c2cef
yovic4 <yovic4@yahoo.co.id>
2021-06-26 11:28:23 +0700

    Add fanart Board

--- packages/frontend/api/fanart.ts
+++ packages/frontend/api/fanart.ts
@@ -5,6 +5,19 @@ const fanartInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 })
 
+export async function getFanart(): Promise<FanartInterface[]> {
+  // Just a temp for getFanart API
+  // Todo : Remove this file when Video API ready
+  const list: FanartInterface[] = []
+  for (let i = 1; i <= 6; i++) {
+    list.push({
+      creator: 'Creator ' + i,
+      imageUrl: `/temp-fanart/${i}.png`,
+    })
+  }
+  return list
+}
+
 export async function createFanart(data: FanartInterface) {
   await fanartInstance.post('/fanart', data)
 }
---
+++ packages/frontend/components/Fanart/FanartBoard.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import FanartCard from './FanartCard'
+import { FanartInterface } from '../../interfaces/fanart'
+import { getFanart } from '../../api/fanart'
+import MasonryBoard from '../MasonryBoard'
+import styles from './styles.module.scss'
+
+export default function FanartBoard() {
+  const [fanarts, setFanarts] = useState<FanartInterface[]>()
+
+  useEffect(() => {
+    async function onStart() {
+      const fanarts = await getFanart()
+      setFanarts(fanarts)
+    }
+
+    onStart()
+  })
+  return (
+    <div className={styles.fanart_board}>
+      <h2>Click on the images to enlarge them!</h2>
+      <MasonryBoard>
+        {fanarts &&
+          fanarts.map((fanart, i) => <FanartCard key={i} {...fanart} />)}
+      </MasonryBoard>
+    </div>
+  )
+}
