6374f2d8ae0dc6fd448d14d840dbb18cde46fd0d
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2021-06-27 21:14:19 -0400

    Changed to take in an array of card data

--- packages/frontend/pages/messages/index.tsx
+++ packages/frontend/pages/messages/index.tsx
@@ -1,51 +1,46 @@
 import React from 'react'
-import messageProps from '../../interfaces/messageProps'
+import cardInfo from '../../interfaces/cardInfo'
 import Footer from '../../components/Footer/Footer'
 import Navigation, { Page } from '../../components/Navigation/Navigation'
 import ToTheTopButton from '../../components/ToTheTopButton'
 import MessageList from '../../components/MessageCards/MessageList'
 
-const messageList: messageProps[] = [
+const Messages: cardInfo[] = [
   {
-    key: '1',
-    cardData: [
-      {
-        title: 'Dear Coco',
-        message:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
-        author: '- Latin Guy ',
-      },
-      {
-        title: 'Dear Coco',
-        message:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
-        author: '-Author',
-      },
-      {
-        title: 'Dear Coco',
-        message:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
-        author: '- A placeholder generator ',
-      },
-      {
-        title: 'Dear Coco',
-        message:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
-        author: '-Author',
-      },
-      {
-        title: 'Dear Coco',
-        message:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
-        author: '-Author',
-      },
-      {
-        title: 'Dear Coco',
-        message:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
-        author: '-Author',
-      },
-    ],
+    title: 'Dear Coco',
+    message:
+      'In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
+    author: '- Latin Guy ',
+  },
+  {
+    title: 'Dear Coco',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
+    author: '-Author',
+  },
+  {
+    title: 'Dear Coco',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
+    author: '- A placeholder generator ',
+  },
+  {
+    title: 'Dear Coco',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
+    author: '-Author',
+  },
+  {
+    title: 'Dear Coco',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
+    author: '-Author',
+  },
+  {
+    title: 'Dear Coco',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac magna velit tellus vulputate. Libero scelerisque mattis ac gravida ut id mattis odio. In quis vel, mattis urna vel volutpat morbi ante vel. Quis condimentum at at ipsum, consequat magna lorem fames morbi.',
+    author: '-Author',
   },
 ]
 
@@ -53,9 +48,7 @@ export default function MessageBoard() {
   return (
     <>
       <Navigation title="Messages" page={Page.MESSAGES} />
-      {messageList.map((messages, i) => (
-        <MessageList key={i} {...messages} />
-      ))}
+      <MessageList key={'1'} cardData={Messages} />
       <ToTheTopButton />
       <Footer />
     </>
