78b09be18be32a0d7041bba59807db872940efbb
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2021-06-29 17:59:15 -0400

    Update packages/frontend/components/MessageCards/MessageList.tsx
    Co-authored-by: azuraga <73472335+azuraga@users.noreply.github.com>

--- packages/frontend/components/MessageCards/MessageList.tsx
+++ packages/frontend/components/MessageCards/MessageList.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import MessageCard from './MessageCard'
 import MessageProps from '../../interfaces/messageProps'
-import styles from './MessageCard.module.scss'
-import MasonryBoard from './MasonryBoard'
+import styles from '../Fanart/styles.module.scss'
+import MasonryBoard from '../MasonryBoard'
 
 export default function MessageList(props: MessageProps) {
   const cards = props.cardData.map((message, i) => (
     <MessageCard key={i} {...message} />
   ))
   return (
-    <div className={styles.container}>
+    <div className={styles.fanart_board}>
       <MasonryBoard> {cards}</MasonryBoard>
     </div>
   )
