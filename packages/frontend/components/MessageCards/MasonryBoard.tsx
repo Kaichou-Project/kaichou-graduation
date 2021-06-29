c1e23167cef98c83dd95fa210b7610c5b5c76051
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2021-06-29 14:11:01 -0400

    Used a masonry board system instead

---
+++ packages/frontend/components/MessageCards/MasonryBoard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import Masonry from 'react-masonry-css'
+
+const breakpointColumnsObj = {
+  default: 2,
+  800: 1,
+}
+
+export default function MasonryBoard({ children }) {
+  return (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className={styles.masonry_grid}
+      columnClassName={styles.masonry_grid_col}
+    >
+      {children}
+    </Masonry>
+  )
+}
