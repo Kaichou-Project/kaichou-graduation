2d41e430ee90425dc36313b3ff13389e15145b3c
josef-yu <slay3r28@gmail.com>
2021-06-30 20:18:48 +0800

    [ADDED] Button to /submission in Fanart page

--- packages/frontend/components/Fanart/FanartBoard.tsx
+++ packages/frontend/components/Fanart/FanartBoard.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import FanartCard from './FanartCard'
 import { FanartInterface } from '../../interfaces/fanart'
 import { getFanart } from '../../api/fanart'
+import Link from 'next/link'
 import MasonryBoard from '../MasonryBoard'
 import styles from './styles.module.scss'
 
@@ -18,6 +19,12 @@ export default function FanartBoard() {
   }, [])
   return (
     <div className={styles.fanart_board}>
+      <div className={styles.header}>
+        <h2>Fanart from the Community</h2>
+        <Link href="/submission">
+          <div className={styles.button}>Submit your own</div>
+        </Link>
+      </div>
       <h2>Click on the images to enlarge them!</h2>
       <MasonryBoard>
         {fanarts &&
