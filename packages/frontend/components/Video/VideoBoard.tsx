408d4c9677c7fc536d0748ce0196c761cc90aad8
josef-yu <slay3r28@gmail.com>
2021-06-30 20:04:49 +0800

    [ADDED] Button to /submission in Video page

--- packages/frontend/components/Video/VideoBoard.tsx
+++ packages/frontend/components/Video/VideoBoard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Video, { VideoInterface } from './Video'
 import styles from './videoBoard.module.scss'
+import Link from 'next/link'
 
 export default function VideoBoard() {
   const [videos] = useState<VideoInterface[]>([
@@ -19,12 +20,18 @@ export default function VideoBoard() {
   ])
 
   return (
-    <>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h2>Videos from the Community</h2>
+        <Link href="/submission">
+          <div className={styles.button}>Submit your own</div>
+        </Link>
+      </div>
       <div className={styles.listContainer}>
         {videos.map((video) => (
           <Video key={video.id} video={video} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
