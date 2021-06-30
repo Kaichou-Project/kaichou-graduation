5e6a80349c948014b94e26a01552338bf68b05ff
yovic4 <yovic4@yahoo.co.id>
2021-06-30 16:20:23 +0700

    Add preview to forms

--- packages/frontend/components/Forms/FormMessage.tsx
+++ packages/frontend/components/Forms/FormMessage.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import TextInput from './TextInput'
 import TextArea from './TextArea'
 import CheckConfirm from './CheckConfirm'
 import SubmitButton from './SubmitButton'
 import { MessageInterface } from '../../interfaces/message'
 import { createMessage } from '../../api/message'
-import { formDataToObject } from '../../utils/formData'
+import { getFormData } from '../../utils/formData'
 import styles from './Form.module.scss'
 
 interface propsInterface {
@@ -18,6 +18,7 @@ interface propsInterface {
 
 interface dataType extends MessageInterface {
   captchaToken: string
+  confirmation?: string
 }
 
 interface errorType {
@@ -29,26 +30,34 @@ interface errorType {
 
 export default function FormMessage(props: propsInterface) {
   const { hidden, captchaToken, onSubmit, onSuccess, onFail } = props
+  const [preview, setPreview] = useState<dataType>(null)
   const [errors, setErrors] = useState<errorType>({})
+  const formEl = useRef(null)
+
+  function validate(data: dataType, showError: boolean): boolean {
+    data.creator = data.creator.trim()
+    if (!data.creator) {
+      if (showError) setErrors({ creator: "This field can't be empty" })
+      return false
+    }
+
+    if (!data.content) {
+      if (showError) setErrors({ content: "This field can't be empty" })
+      return false
+    }
+
+    return true
+  }
 
   async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
     evt.preventDefault()
 
-    const formData = new FormData(evt.currentTarget)
+    const data = getFormData(formEl.current) as dataType
 
-    const confirmation = !!formData.get('confirmation')
-    formData.delete('confirmation')
+    if (!validate(data, true)) return
 
-    const data = formDataToObject(formData) as dataType
-
-    data.creator = data.creator.trim()
-    if (!data.creator) {
-      return setErrors({ creator: "This field can't be empty" })
-    }
-
-    if (!data.content) {
-      return setErrors({ content: "This field can't be empty" })
-    }
+    const confirmation = !!data.confirmation
+    delete data.confirmation
 
     if (!confirmation) {
       return setErrors({ confirmation: 'You must confirm this' })
@@ -73,16 +82,29 @@ export default function FormMessage(props: propsInterface) {
     }
   }
 
+  function handleChange() {
+    const data = getFormData(formEl.current) as dataType
+    if (validate(data, false)) setPreview(data)
+    else setPreview(null)
+  }
+
   return (
     <form
+      ref={formEl}
       className={`${styles.form} ${hidden ? styles.hide : ''}`}
       onSubmit={handleSubmit}
     >
-      <TextInput name="creator" label="My name is ..." error={errors.creator} />
+      <TextInput
+        name="creator"
+        label="My name is ..."
+        onChange={handleChange}
+        error={errors.creator}
+      />
 
       <TextArea
         name="content"
         label="My message to Coco is ...."
+        onChange={handleChange}
         error={errors.content}
       />
 
