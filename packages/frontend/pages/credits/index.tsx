8cebfcabe0e215b69179c979fe3333730af53cea
keijeizei <53002096+keijeizei@users.noreply.github.com>
2021-06-30 23:28:05 +0800

    Add partial list of developers

--- packages/frontend/pages/credits/index.tsx
+++ packages/frontend/pages/credits/index.tsx
@@ -4,18 +4,19 @@ import Footer from '../../components/Footer/Footer'
 import styles from './index.module.scss'
 
 export default function Credits() {
-  const builders = [
-    'name1',
-    'name2',
-    'name1',
-    'name2',
-    'name1',
-    'name2',
-    'name1',
-    'name2',
-    'name1',
-    'name2',
+  const head_devs = ['RedMaps', 'Nyxordinal', 'Azuraga Melody', 'PS-Soundwave']
+  const developers = [
+    'FomTarro (Tom "Skeletom" Farro)',
+    'GlennPoh',
+    'H-isaac23',
+    'keijeizei',
+    'MeckiMesser',
+    'Reinhek',
+    'Vince14Genius',
+    'VeryCoolMoment',
+    'yovic',
   ]
+  const designers = ['RedMaps']
 
   return (
     <div className={styles.credits}>
@@ -28,7 +29,10 @@ export default function Credits() {
         five centuries, but also the leap into electronic typesetting, remaining
         essentially unchanged.
       </p>
-      <h2 className={styles.title}>Project 1</h2>
+
+      <hr />
+
+      <h2 className={styles.title}>Coco&apos;s Graduation Farewell</h2>
       <p className={styles.content}>
         All about project 1. Lorem Ipsum is simply dummy text of the printing
         and typesetting industry. Lorem Ipsum has been the industrys standard
@@ -36,10 +40,63 @@ export default function Credits() {
         of type and scrambled it to make a type specimen book. It has survived
         not only five centuries
       </p>
+      <h2 className={styles.title}>Operation Sleeping Dragon</h2>
+      <p className={styles.content}>
+        All about project 1. Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industrys standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries
+      </p>
+      <h2 className={styles.title}>Cinderella Project</h2>
+      <p className={styles.content}>
+        All about project 1. Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industrys standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries
+      </p>
+      <h2 className={styles.title}>Kiryukai Project</h2>
+      <p className={styles.content}>
+        All about project 1. Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industrys standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries
+      </p>
+      <h2 className={styles.title}>Kaichou Project</h2>
+      <p className={styles.content}>
+        All about project 1. Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industrys standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries
+      </p>
+
       <h3 className={styles.roles}>Head Developers</h3>
-      <div className={`${styles.members} ${styles.content}`}>
-        {builders.map((member, i) => (
-          <p key={i}>{member}</p>
+      <div className={styles.members}>
+        {head_devs.map((name, i) => (
+          <p className={styles.member} key={i}>
+            {name}
+          </p>
+        ))}
+      </div>
+
+      <h3 className={styles.roles}>Developers</h3>
+      <div className={styles.members}>
+        {developers.map((name, i) => (
+          <p className={styles.member} key={i}>
+            {name}
+          </p>
+        ))}
+      </div>
+
+      <h3 className={styles.roles}>Designers</h3>
+      <div className={styles.members}>
+        {designers.map((name, i) => (
+          <p className={styles.member} key={i}>
+            {name}
+          </p>
         ))}
       </div>
       <Footer />
