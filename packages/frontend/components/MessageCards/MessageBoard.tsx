643a88adc63200a043f8c3db61daa5ddd7c8fc21
josef-yu <slay3r28@gmail.com>
2021-07-01 21:12:01 +0800

    [ADDED] Button to /submission in Messages page

--- packages/frontend/components/MessageCards/MessageBoard.tsx
+++ packages/frontend/components/MessageCards/MessageBoard.tsx
@@ -5,6 +5,7 @@ import { getMessages } from '../../api/message'
 import InfiniteScrolling from '../InfiniteScrolling'
 import styles from '../Fanart/styles.module.scss'
 import MasonryBoard from '../MasonryBoard'
+import Link from 'next/link'
 
 const breakpointColumnsObj = {
   default: 3,
@@ -17,6 +18,12 @@ export default function MessageBoard() {
 
   return (
     <div className={styles.fanart_board}>
+      <div className={styles.header}>
+        <h2>Messages from the Community</h2>
+        <Link href="/submission">
+          <div className={styles.button}>Submit your own</div>
+        </Link>
+      </div>
       <InfiniteScrolling
         next={getMessages}
         onData={(data) => setMessages(data as MessageResponseInterface[])}
