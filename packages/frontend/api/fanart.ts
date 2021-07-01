2eb34b744ddbf0a92c19ec2a65ed9f4ccd95bb71
Nyxordinal <kujyokazunejpn@gmail.com>
2021-07-01 12:10:41 +0700

    combine axios instances on the frontend into one and add handling if it gets an error response

--- packages/frontend/api/fanart.ts
+++ packages/frontend/api/fanart.ts
@@ -1,27 +1,39 @@
+import { AxiosResponse } from 'axios'
+import { APIResponse } from '../interfaces/api'
 import { FanartInterface, FanartResponseInterface } from '../interfaces/fanart'
-import axios from 'axios'
-
-const fanartInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-})
+import AxiosInstance from './axios'
 
 export async function getFanart(
   limit?: number,
   lastId?: string
 ): Promise<FanartResponseInterface[]> {
   try {
-    const res = await fanartInstance.get('/fanart', {
-      params: {
-        limit,
-        lastId,
-      },
-    })
+    const res: AxiosResponse<APIResponse<FanartResponseInterface[]>> =
+      await AxiosInstance.get('/fanart', {
+        params: {
+          limit,
+          lastId,
+        },
+      })
     return res.data.content
   } catch (err) {
-    throw TypeError(err.response.data.message)
+    if (err.response.data) {
+      throw TypeError(err.response.data.message)
+    }
+    throw TypeError(err.message)
   }
 }
 
 export async function createFanart(data: FanartInterface) {
-  await fanartInstance.post('/create/fanart', data)
+  try {
+    await AxiosInstance.post('/create/fanart', data)
+  } catch (error) {
+    if (error.response.status === 429) {
+      throw new TypeError('Submission limit reached. Try again in an hour.')
+    }
+    if (error.response.data) {
+      throw new TypeError(error.response.data.message)
+    }
+    throw new TypeError(error.message)
+  }
 }
